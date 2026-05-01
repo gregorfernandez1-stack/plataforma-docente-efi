98796a3583e328f204a2d6367027eb2b469ace74
Gregor Fernandez <gregorfernandez1@gmail.com>
2026-05-01 00:03:11 -0400

    fix favicon definitivo

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,14 +17,8 @@ export const metadata = {
   description:
     "Sistema para la creación, gestión y organización de planificaciones docentes en Educación Física.",
   icons: {
-    icon: [
-      { url: "/favicon.ico" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-    ],
-    apple: "/apple-touch-icon.png",
+    icon: "/favicon.ico",
   },
-   manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
